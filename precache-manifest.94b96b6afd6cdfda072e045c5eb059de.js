b47d51b6686c513b91178fc96de4ef08b66cdf3e
Filip Kozlik <filip.kozlik.public@gmail.com>
2020-02-03 22:28:59 +0100

    Updates

---
+++ precache-manifest.94b96b6afd6cdfda072e045c5eb059de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac02e304ea58b0793f8c9e382a34c0c4",
+    "url": "/WeddingInvite/index.html"
+  },
+  {
+    "revision": "634de8a1c2ae03f820d5",
+    "url": "/WeddingInvite/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f61b3bbf4a18944897c1",
+    "url": "/WeddingInvite/static/js/2.69f5c362.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/WeddingInvite/static/js/2.69f5c362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "634de8a1c2ae03f820d5",
+    "url": "/WeddingInvite/static/js/main.f1d38d9e.chunk.js"
+  },
+  {
+    "revision": "5cb9921b28ead57d7c97",
+    "url": "/WeddingInvite/static/js/runtime-main.94cab858.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WeddingInvite/static/media/logo.5d5d9eef.svg"
+  }
+]);
